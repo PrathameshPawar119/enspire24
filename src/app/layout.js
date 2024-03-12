3442993ea30a4bac7dce4cdab9c59fea0780d692
PrathameshPawar119 <cm.b26.prathameshpawar@gmail.com>
2024-03-13 02:00:14 +0530

    indexing

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="title" content="Enspire - Flagship Event" />
+        <meta name="description" content="Enspire is the flagship event of Ecell DMCE (Entrepreurship Cell - Datta Meghe College of Engineering)" />
+
+        <meta name="google-site-verification" content="60_7SYi3TgmDiIlLJX2kG8Yb3Z3WhnSe-S42aI1qhSM" />
+      </head>
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
